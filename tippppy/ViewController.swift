4bb0c23617eef0c8a3d94bcd39d9dfa3fefce828
Siddhant Patel <si12hant@gmail.com>
2017-12-23 11:55:55 +0530

    test

---
+++ tippppy/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  tippppy
+//
+//  Created by siddhant on 12/21/17.
+//  Copyright © 2017 siddhant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipController: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+
+    @IBAction func valueChanged(_ sender: Any) {
+        calculateTip(self)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18,0.2,0.25]
+        
+        let bill = Double(billField.text!)! ?? 0
+        let tip = bill * tipPercentages[tipController.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
+        
+        
+        
+        
+    }
+    
+}
+
